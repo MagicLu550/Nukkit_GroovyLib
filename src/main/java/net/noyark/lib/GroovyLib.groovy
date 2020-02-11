4b9a96573b011faa95776715201971e32b8174fc
magiclu550 <843983728@qq.com>
2020-02-11 16:31:39 +0800

    Initial commit

---
+++ src/main/java/net/noyark/lib/GroovyLib.groovy
@@ -0,0 +1,11 @@
+package net.noyark.lib
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class GroovyLib extends JavaPlugin{
+
+    @Override
+    void onEnable() {
+        this.logger.info("加载groovy lib")
+    }
+}
